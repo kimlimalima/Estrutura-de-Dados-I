187632c526a6563d05cd91d257b4c23390de24a1
Kim Lima <86325116+kimlimalima@users.noreply.github.com>
2023-03-17 23:21:38 +0000

    inputinho

--- iotput.cpp
+++ iotput.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
 
 using namespace std;
+// Imput
+string nome;
+int idade;
+char sexo;
+double contaBancaria;
+float scoreSerasa;
 
 int main() {
-  // Imput
-  int age;
-  cin >> age;
 
   // Output
-  cout << "Idade: " << age << "." << endl;
-  printf("Idade: %i. \n", age);
+  cout << "Qual teu nome?" << endl;
+  cin >> nome;
+  getline(cin, nome);
+
+  cout << "Qual tua idade?" << endl;
+  cin >> idade;
+
+  cout << "M or F?" << endl;
+  cin >> sexo;
+
+  cout << "Triste ou feliz? Quero números, já sabeee." << endl;
+  cin >> contaBancaria;
+
+  cout << "Me diga em números, o que tira teu sono: " << endl;
+  cin >> scoreSerasa;
+
+  cout << "Nome: " << nome << "." << endl;
+  cout << "Idade: " << idade << "." << endl;
+  cout << "Sexo: " << sexo << "." << endl;
+  cout << "Valor na conta bancária: " << contaBancaria << "." << endl;
+  cout << "Score no serasa: " << scoreSerasa << "." << endl;
 
   return 0;
 }
