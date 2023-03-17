14fc2ccd190a04c266643b83e2896fc4002d85c7
Kim Lima <86325116+kimlimalima@users.noreply.github.com>
2023-03-17 21:52:55 +0000

    Verificando criação de executáveis. De quebra toma um I/O pra tu.

---
+++ .ccls-cache/@home@runner@Estrutura-de-Dados-I/iotput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // Imput
+  int age;
+  cin >> age;
+
+  // Output
+  cout << "Idade: " << age << "." << endl;
+  printf("Idade: %i. \n", age);
+
+  return 0;
+}
