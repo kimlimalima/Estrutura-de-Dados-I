ada95866a3875dd409fca0d571fb71f65c2893ae
Kim Lima <mikamil747@gmail.com>
2023-03-27 14:29:35 -0300

    Implementando ponteiros

---
+++ RevisaoCPP/ponteiros.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int total;
+    int *intPointer = new int;
+    
+    total = *intPointer;
+    total = 25;
+    delete intPointer;
+    cout << total << endl << *intPointer << endl;
+    return 0;
+}
